f20f62f19535864d645a99acdddd396d96266478
Rajjada001 <Rajjada001@gmail.com>
2022-02-14 22:27:29 +0530

    Added an another optimised approach

--- Strings/2.Longest_common_prefix.py
+++ Strings/2.Longest_common_prefix.py
@@ -7,6 +7,8 @@ def longest_common_prefix(s):
     if s=="": return ""
 
     # get the minimum value in s by the size. 
+    # Approach 1:
+    '''
     shortest = min(s, key=len)
     print(min(s))
     print(shortest)
@@ -16,9 +18,24 @@ def longest_common_prefix(s):
                 return shortest[:i]
 
     return shortest
+    '''
 
+    # Approach 2:
+    # We just need to compare only maximum and minimum length elements. 
+    # If there are any common points, keep iterating.
+    # Once the elements mismatched, we will return the array till the mis matched element.
+    s1,s2 = min(s),max(s)
+    print(s1,s2)
+    i = 0
+    while(i<len(s1)):
+        if s1[i] != s2[i]:
+            return s1[:i]
+        i+=1
+    
+    return ""
 
 
 print(longest_common_prefix(["flower","flow","flight"]))
 print(longest_common_prefix(["abcd", "accd", "adcd"]))
 print(longest_common_prefix(["leetcode", "lee", "lead"]))
+print(longest_common_prefix(["dog","racecar","car"]))
